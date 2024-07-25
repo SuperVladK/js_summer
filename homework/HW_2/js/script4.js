1c8efce3b134f760acbad4e4580c0aef427996e8
SuperVladK <128508632+SuperVladK@users.noreply.github.com>
2024-07-25 10:53:15 +0300

    Add files via upload

--- homework/HW_2/js/script4.js
+++ homework/HW_2/js/script4.js
@@ -5,4 +5,4 @@ const santimeterInMeter = 1 / 100
 const santimeterInKilometer = santimeterInMeter / 1000
 const lengthInMeter = santimeterLength * santimeterInMeter
 const lengthInKilometer = santimeterLength * santimeterInKilometer
-document.write(`${santimeterLength} см = ${lengthInMeter.toFixed(2)} м та ${lengthInKilometer.toFixed(5)} км`)
+document.write(`${santimeterLength} см = ${lengthInMeter.toFixed(2)} м або ${lengthInKilometer.toFixed(5)} км`)
