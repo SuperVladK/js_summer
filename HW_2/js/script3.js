eb4bcae4da275ad80c2ef06451ebb3649c9ba6af
SuperVladK <128508632+SuperVladK@users.noreply.github.com>
2024-07-25 10:43:26 +0300

    Delete HW_2 directory

--- HW_2/js/script3.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-let itemPrize = parseFloat(prompt('Введіть вартість товару у грн'))
-let itemCount = parseInt(prompt('Введіть кількість товару'))
-const vat = 5 / 100
-let totalPrize = itemPrize * itemCount
-let totalVat = totalPrize * vat
-document.write(`Загальна вартість товару складає ${totalPrize.toFixed(2)} грн <br> ПДВ складає ${totalVat.toFixed(2)} грн`)
--- HW_2/js/script4.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-let santimeterLength = parseInt(prompt('Введіть довжину у сантиметрах'))
-const santimeterInMeter = 1 / 100
-const santimeterInKilometer = santimeterInMeter / 1000
-const lengthInMeter = santimeterLength * santimeterInMeter
-const lengthInKilometer = santimeterLength * santimeterInKilometer
-document.write(`${santimeterLength} см = ${lengthInMeter.toFixed(2)} м та ${lengthInKilometer.toFixed(5)} км`)
