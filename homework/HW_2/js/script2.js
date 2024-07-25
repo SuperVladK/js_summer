96e9c8fb94066bb2f73a064b57321a20f426032e
SuperVladK <128508632+SuperVladK@users.noreply.github.com>
2024-07-25 10:34:59 +0300

    Delete homework directory

--- homework/HW_2/js/script2.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-let birthYear = parseFloat(prompt('Введіть рік Вашого народження'))
-let currentYear = parseFloat(prompt('Введіть поточний рік'))
-const ammountOfYears = currentYear - birthYear
-document.write(`Ваш вік на сьогоднійшній день складає ${ammountOfYears} повних років`)
--- homework/HW_2/js/script5.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-let secondNum = parseInt(prompt('Введіть кількіть секунд'))
-const secondInMinute = 1 / 60
-const secondInHour = secondInMinute / 60
-const minutePassed = secondNum * secondInMinute
-const hourPassed = secondNum * secondInHour
-document.write(`З початку доби пройшло ${secondNum} секунд, що складає ${minutePassed.toFixed(2)} хвилин або ${hourPassed.toFixed(4)} годин.`)
