e8dd34924360febe6449f97233d0eafb0d701856
SuperVladK <128508632+SuperVladK@users.noreply.github.com>
2024-07-30 17:05:37 +0300

    Add files via upload

--- homework/HW_3/js/script2.js
+++ homework/HW_3/js/script2.js
@@ -3,9 +3,9 @@
 let itemPrice = parseFloat(prompt('Введіть ціну товару'))
 let moneyAmount = parseFloat(prompt('Введіть наявну кількість грошей'))
 let moneyLeft = moneyAmount - itemPrice
-if (moneyAmount > itemPrice || moneyAmount === itemPrice) {
+if (moneyAmount >= itemPrice) {
    document.write('Вітаємо з придбанням чудового товару!<br>') 
-   if (moneyLeft > 4 || moneyLeft === 4)
+   if (moneyLeft >= 4)
       document.write('Можливо купимо ще лотерейку і Вам пощастить?')
    else if (moneyLeft < 4) 
       document.write('Нажаль ще на лотерейку Вам не вистачає.')
--- homework/HW_3/js/script7.js
+++ homework/HW_3/js/script7.js
@@ -7,11 +7,11 @@ const autumn = 'осінь'
 const winter = 'зима'
 if (monthNum === 12 || monthNum < 3) 
    document.write(`Ваша пора року ${winter}`)
-else if (monthNum > 2 && monthNum < 6)
+else if (monthNum < 6)
    document.write(`Ваша пора року ${spring}`)
-else if (monthNum > 5 && monthNum < 9)
+else if (monthNum < 9)
    document.write(`Ваша пора року ${summer}`)
-else if (monthNum > 8 && monthNum < 12)
+else if (monthNum < 12)
    document.write(`Ваша пора року ${autumn}`)
 else 
    document.write('Ви ввели некоректне значення')
