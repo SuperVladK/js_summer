d86869124aebea8b4740b02e76371d34b0b3f1dd
SuperVladK <128508632+SuperVladK@users.noreply.github.com>
2024-08-20 15:14:11 +0300

    Add files via upload

--- homework/js/script.js
+++ homework/js/script.js
@@ -1,6 +1,5 @@
 "use strict"
 
-
 // for (let i = 1; i <= 5; i++) {
 //    let numOne = parseInt(prompt('num1'))
 //    let numTwo = parseInt(prompt('num2'))
@@ -137,3 +136,94 @@
 // }
 // while (money < phonePrice)
 // document.write(`Collected!<br>Helen has ${money} $ and can buy the phone!`)
+
+// document.write(`<container class="vertical-diagramm">`)
+// for (let i = 1; i <= 8; i++){
+// let width = 1 + Math.floor(Math.random()*500)
+//    document.write(`<div class="diagramm" style="height:${width}px">${width}</div>`)
+// }
+// document.write(`</container>`)
+
+// for (let i = 1; i <= 10; i++){
+//    let left = 1 + Math.floor(Math.random() * 1200)
+//    let top = 1 + Math.floor(Math.random() * 800)
+//    document.write(`<img class="smile" style="position:absolute; left:${left}px; top:${top}px"  src="../HW_5/img/smile.webp">`)
+// }
+
+// for (let min = 1, max = 10; min <= 91; min += 10, max += 10) {
+//    document.write(`<hr>`)
+//    for (let i = 1; i <= 3; ) {
+//       let randomNum = 1 + Math.floor(Math.random() * (max - min + 1))
+//       if (randomNum % 2 !== 0) continue
+//       i++
+//       document.write(`${randomNum}-`)
+//    }
+// }
+
+// let minYear = parseInt(prompt('Введіть мінімальний рік діапазону'))
+// let maxYear = parseInt(prompt('Введіть максимальний рік діапазону'))
+// for (let num = minYear; num <= maxYear; num++) {
+//    if (num % 4 === 0 && num % 100 !== 0 || num % 400 === 0) 
+//    document.write(`${num}<br>`)
+// }
+
+// for (let c1 = 1; c1 <= 9; c1++)
+//    for (let c2 = 0; c2 <= c1; c2++)
+//       for (let c3 = 0; c3 <= c2; c3++)
+//          document.write(`${c1}${c2}${c3}<br>`)
+
+// document.write(`<select>`)
+// for (let num = 0; num <= 20; num+=2)
+//    document.write(`<option value="${num}">${num}</option>`)
+// document.write(`</select>`)
+
+// let firstChar = prompt('Перша буква')
+// let secondChar = prompt('Друга буква')
+// let firstCharCode = firstChar.charCodeAt(0)
+// let secondCharCode = secondChar.charCodeAt(0)
+// document.write(`<select>`)
+// for (let code = firstCharCode; code <= secondCharCode; code++) {
+//    document.write(`<option value="${code}">${code}</option>`)
+// }
+// document.write(`</select>`)
+// document.write(`<select>`)
+// for (let char = firstCharCode; char <= secondCharCode; char++){
+//    document.write(`<option value="${char}">${String.fromCharCode(char)}</option>`)
+// }
+// document.write(`</select>`)
+
+// function isPensioner(age, pensioner) {
+//    return age>=pensioner
+// }
+// let userAge = parseInt(prompt('Введіть Ваш вік.'))
+// let msg = isPensioner(userAge, 65) ? 'Rest' : 'Work'
+// document.write(`${msg}`)
+
+// let prices = []
+// let pricesNum = parseInt(prompt('Add num'))
+// for (let i = 0; i < pricesNum; i++){
+//    let price = parseFloat(prompt('Add price'))
+//    prices.push(price)
+//    console.log(prices)
+// }
+// let pricesSum = 0
+// for (let a = 0; a < prices.length; a++){
+//    pricesSum+= prices[a]
+// }
+// document.write(`${pricesSum}`)
+
+// if (confirm('Почати тестування')) {
+//    let pupilNamesList = []
+//    let pupilNamesListQnt = parseInt(prompt('Скільки треба ввести імен учнів?'))
+//    for (let i = 1; i <= pupilNamesListQnt; i++) {
+//       let pupilName = prompt(`Введіть імʼя ${i} учня`)
+//       pupilNamesList.push(pupilName)
+//    }
+//    console.log(pupilNamesList)
+//    let nameIvanQnt = 0
+//    for (let a = 1; a <= pupilNamesList.length; a++) {
+//       if (pupilNamesList[a] === 'Ivan') nameIvanQnt++
+//    }
+//    document.write(`В класі ${nameIvanQnt} Іванів.`)
+// }
+
